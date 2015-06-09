8299848d9d88143e0cbf1ef5da6ee6470d5468e4
sudheepdivakargithub <sudheepdivakar@gmail.com>
2015-06-09 15:56:45 +0530

    added code to spec

--- src/InstrumentSpec.java
+++ src/InstrumentSpec.java
@@ -1,4 +1,19 @@
 
 public abstract class InstrumentSpec {
 
+	private Builder builder;
+	private String model;
+	private Type type;
+	private Wood backWood;
+	private Wood topWood;
+	
+	public InstrumentSpec(Builder builder, String model, Type type, Wood backWood, Wood topWood)
+	{
+	this.builder = builder;
+	this.model = model;
+	this.type = type;
+	this.backWood = backWood;
+	this.topWood = topWood;		
+	}
+	
 }
