e26c38107d574b29907e8745c9e640fb3164f51c
sudheepdivakargithub <sudheepdivakar@gmail.com>
2015-06-09 16:05:37 +0530

    added the mathes function in the spec

--- src/InstrumentSpec.java
+++ src/InstrumentSpec.java
@@ -16,4 +16,20 @@ public abstract class InstrumentSpec {
 	this.topWood = topWood;		
 	}
 	
+	public boolean matches(InstrumentSpec otherSpec){
+		if(builder != otherSpec.builder)
+			return false;
+		if((model!=null) && !model.equals("") && (!model.equals(otherSpec.model)))
+		return false;
+		if(type!= otherSpec.type)
+			return false;
+		if(backWood != otherSpec.backWood)
+			return false;
+		if(topWood != otherSpec.topWood)
+			return false;
+		
+		return true;
+		
+		
+	}
 }
